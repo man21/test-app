8c20acb0350e7c9b1e4be89ed706c8a369ee54fb
mann21 <yadavmandeep96@gmail.com>
2024-12-02 01:13:49 +0530

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 // Route to serve static text
 app.get('/', (req, res) => {
 
-  res.send('<h1>Hello, V11 </h1>');
+  res.send('<h1>Hello, V12 </h1>');
 });
 
 // Start the server
